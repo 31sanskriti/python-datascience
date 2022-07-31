02485d12bc712702dcc5be3bd0245c8941be0d36
31sanskriti <110278708+31sanskriti@users.noreply.github.com>
2022-07-31 14:04:51 +0530

    basics

---
+++ datatype.py
@@ -0,0 +1,38 @@
+x = 1999
+y = 2000
+z = -5693
+y2 = 1998
+x2 =  1.10
+y3 = 87.7e100
+z2 = -35.59
+print (x, type(x))
+print (y, type(y))
+print (z, type(z))
+print (y2, type(y2))
+print (x2, type(x2))
+print (y3, type(y3))
+print (z2, type(z2))
+
+a ="hello"
+print (a, type(a))
+n = "sanskriti" 
+c = "black"
+i = "digipodium"
+print (n, type(n))
+print (c, type(c))
+print (i, type(i))
+
+colors = ["red","green","black"]
+print (colors, type(colors))
+Q = ["1,3,4,56,78,90"]
+print (Q, type(Q))
+
+k = {"23'45, 56,78,"}
+print (k, type(k))
+l = {
+    "brand": "ford",
+    "model": "mustang",
+    "year" : "1964"
+}
+print (l, type(l))
+
---
+++ operators.py
@@ -0,0 +1,17 @@
+a= 10
+b=8
+
+print(a + b)
+print (a - b)
+print (a * b)
+print (a / b) #float division
+print (a // b) # integer division (quotient)
+print (a % b)  # remainder
+print (a ** b) # exponent
+
+#operator precedence 
+
+fruits = ['apple' , 'orange' , 'pineapple' , 'pear' , 'grapes' , 'blueberry','cherry']
+print ('apple' in fruits)
+print ('pineapple' in fruits)
+print ('grapes' in fruits)
